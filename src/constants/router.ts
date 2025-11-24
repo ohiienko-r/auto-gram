963f4b7fa45639e1bc8278d72a386c4162c3a5bb
Roman Ohiienko <ohiienko.r@gmail.com>
2025-11-24 23:14:52 +0200

    feat: add routes names to constants

---
+++ src/constants/router.ts
@@ -0,0 +1,9 @@
+export const ROUTES_NAMES = {
+  ROOT: "/",
+  DETAILS: "/details",
+  SEARCH: "/search",
+  FAVORITES: "/favorites",
+  PROFILE: "/profile",
+  CREATE_LISTING: "/create-listing",
+  EDIT_LISTING: "/edit-listing",
+} as const;
