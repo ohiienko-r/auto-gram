b152e27d49119fbd18e8b03e19c6edbe5487e7a3
Roman Ohiienko <ohiienko.r@gmail.com>
2025-11-27 22:22:01 +0200

    feat: add Toggle and Input components; update SearchForm with new form structure and styling

--- src/app/SearchPage/forms/SearchForm.tsx
+++ src/app/SearchPage/forms/SearchForm.tsx
@@ -1,15 +1,104 @@
+import { useForm } from "react-hook-form";
+
+import {
+  Form,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Toggle } from "@/components/ui/toggle";
 import RangeSlider from "react-range-slider-input";
 import "react-range-slider-input/dist/style.css";
 
 export default function SearchForm() {
+  const form = useForm({
+    defaultValues: {
+      priceRange: [0, 75] as [number, number],
+      possibleBargain: false,
+      typeofTransport: "",
+    },
+  });
+
+  const { control } = form;
+
   return (
-    <form>
-      <RangeSlider
-        id="range-input"
-        onInput={(val) => {
-          console.log(val);
-        }}
-      />
-    </form>
+    <Form {...form}>
+      <form className="">
+        <div className="flex flex-col gap-3">
+          <FormField
+            control={control}
+            name="priceRange"
+            render={({ field }) => {
+              (
+                document.querySelector(
+                  "#range-input .range-slider__thumb[data-lower]"
+                ) as HTMLElement | null
+              )?.style.setProperty("--slider-value", `"$${field.value[0]}"`);
+
+              (
+                document.querySelector(
+                  "#range-input .range-slider__thumb[data-upper]"
+                ) as HTMLElement | null
+              )?.style.setProperty("--slider-value", `"$${field.value[1]}"`);
+
+              return (
+                <FormItem className="gap-8">
+                  <FormLabel>Ціна</FormLabel>
+
+                  <div className="flex flex-col gap-5">
+                    <RangeSlider
+                      value={field.value}
+                      id="range-input"
+                      defaultValue={[0, 75]}
+                      onInput={field.onChange}
+                    />
+
+                    <div className="gap-3 grid grid-cols-2">
+                      <Input
+                        placeholder="Від"
+                        value={field.value[0]}
+                        onChange={(e) =>
+                          field.onChange([e.target.value, field.value[1]])
+                        }
+                      />
+
+                      <Input
+                        placeholder="До"
+                        value={field.value[1]}
+                        onChange={(e) =>
+                          field.onChange([field.value[0], e.target.value])
+                        }
+                      />
+                    </div>
+                  </div>
+
+                  <FormMessage />
+                </FormItem>
+              );
+            }}
+          />
+
+          <FormField
+            control={control}
+            name="possibleBargain"
+            render={({ field }) => (
+              <FormItem className="justify-items-start">
+                <FormControl>
+                  <Toggle
+                    pressed={field.value}
+                    onPressedChange={field.onChange}
+                  >
+                    Можливен торг
+                  </Toggle>
+                </FormControl>
+              </FormItem>
+            )}
+          />
+        </div>
+      </form>
+    </Form>
   );
 }
---
+++ src/components/ui/toggle.tsx
@@ -0,0 +1,46 @@
+import * as React from "react";
+import * as TogglePrimitive from "@radix-ui/react-toggle";
+import { cva, type VariantProps } from "class-variance-authority";
+
+import { cn } from "@/lib/utils";
+
+const toggleVariants = cva(
+  "inline-flex items-center justify-center gap-2 cursor-pointer transition-colors rounded-[12px] text-base font-medium disabled:pointer-events-none disabled:opacity-50 data-[state=on]:bg-primary data-[state=on]:text-white [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 [&_svg]:shrink-0 focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] outline-none transition-[color,box-shadow] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive whitespace-nowrap",
+  {
+    variants: {
+      variant: {
+        default: "bg-grey",
+        outline:
+          "border border-input bg-transparent shadow-xs hover:bg-accent hover:text-accent-foreground",
+      },
+      size: {
+        default: "h-[35px] px-2",
+        sm: "h-8 px-1.5 min-w-8",
+        lg: "h-10 px-2.5 min-w-10",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+      size: "default",
+    },
+  }
+);
+
+function Toggle({
+  className,
+  variant,
+  size,
+  ...props
+}: React.ComponentProps<typeof TogglePrimitive.Root> &
+  VariantProps<typeof toggleVariants>) {
+  return (
+    <TogglePrimitive.Root
+      data-slot="toggle"
+      className={cn(toggleVariants({ variant, size, className }))}
+      {...props}
+    />
+  );
+}
+
+// eslint-disable-next-line react-refresh/only-export-components
+export { Toggle, toggleVariants };
